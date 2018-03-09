102ad24868dc18c6c1d7b51fdd410e23bca899bf
drdynscript <drdynscript@gmail.com>
2018-03-09 11:51:56 +0100

    Add post model to core and use it in posts page component

--- src/app/posts/pages/posts-page/posts-page.component.ts
+++ src/app/posts/pages/posts-page/posts-page.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Post } from '../../../core/models/post';
+
 @Component({
   selector: 'app-posts-page',
   templateUrl: './posts-page.component.html',
   styleUrls: ['./posts-page.component.scss']
 })
 export class PostsPageComponent implements OnInit {
+  post: Post;
 
   constructor() { }
 
   ngOnInit() {
+    const post = new Post();
+    post.id = 'kjdghkfdgjsrldkghfxjgmldfk';
+    post.title = 'jd dskkds!ds ndosdsdsj dsjdhsjd';
+    this.post = post;
   }
 
 }
